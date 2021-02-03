50b6d64c63c950f5f8da4410c912315e916fa659
deddyprianto <haicoders12@gmail.com>
2021-02-03 14:42:00 +0700

    APP VUE JS PERTAMA KALI

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,30 @@
 <template>
-  <h1>Deddy Reaction Timmer</h1>
-  <button @click="calback">Play</button>
+  <Block :delay="delay" v-if="isPlaying" @end="ending" />
+  <button @click="start" :disabled="isPlaying">
+    Play
+  </button>
+  <p v-if="isPlaying">Your score: {{ scored }}</p>
 </template>
 <script>
+import Block from "./components/Block.vue";
 export default {
   name: "App",
-  components: {},
+  components: { Block },
   data: function() {
     return {
       isPlaying: false,
       delay: null,
+      scored: null,
     };
   },
   methods: {
     start() {
+      this.delay = 2000 + Math.random * 5000;
       this.isPlaying = true;
+    },
+    ending(reactionTimer) {
+      this.scored = reactionTimer;
+      this.isPlaying = false;
     },
   },
 };
@@ -39,5 +49,12 @@ export default {
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+.text {
+  color: #42b983;
+  transition: 0.3s;
+}
+.change__text {
+  color: red;
 }
 </style>
---
+++ src/components/Block.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="block" v-if="jalankan">
+    <h1>haii</h1>
+  </div>
+  <button @click="stopMain">Stoped</button>
+</template>
+
+<script>
+export default {
+  props: ["delay"],
+  data() {
+    return {
+      jalankan: false,
+      waktuini: null,
+      reactionTimer: 0,
+    };
+  },
+  mounted() {
+    console.log(`component mounted`);
+    setTimeout(() => {
+      this.jalankan = true;
+      this.mulaimain();
+    }, this.delay);
+  },
+  methods: {
+    mulaimain() {
+      this.waktuini = setInterval(() => {
+        this.reactionTimer += 10;
+      }, 10);
+    },
+    stopMain() {
+      clearInterval(this.waktuini);
+      this.$emit("end", this.reactionTimer);
+    },
+  },
+};
+</script>
+
+<style>
+.block {
+  width: 400px;
+  border-radius: 20px;
+  background-color: #0faf87;
+  color: white;
+  text-align: center;
+  padding: 100px 0;
+  margin: 40px auto;
+}
+</style>
